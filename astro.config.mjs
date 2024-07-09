d0ded7db89779791e3caf5c8d3b9e6001b9e0d61
Jeedug <131798215+Jeedug@users.noreply.github.com>
2024-07-09 15:48:06 -0500

    added server

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,9 +7,9 @@ import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'hybrid',
-  adapter: node({
-    mode: 'standalone'
-  }),
+  output: 'server',
+  // adapter: node({
+  //   mode: 'standalone'
+  // }),
   integrations: [tailwind(), react()]
 });
