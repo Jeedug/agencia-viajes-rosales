e00522239d41306b365395368cf5f69b0c14c860
Jeedug <131798215+Jeedug@users.noreply.github.com>
2024-07-10 15:05:56 -0500

    node

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,15 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import netlify from '@astrojs/netlify';
+import node from '@astrojs/node';
 
 import db from "@astrojs/db";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'hybrid',
-  adapter: netlify(),
+  adapter: node({
+    mode: 'standalone'
+  }),
   integrations: [tailwind(), react(), db()]
 });
