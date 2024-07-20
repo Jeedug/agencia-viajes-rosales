3a341181c30dae8370895dcaaa67a6ed90fad924
Jeedug <131798215+Jeedug@users.noreply.github.com>
2024-07-19 19:40:30 -0500

    images fixed

--- src/pages/api/images/readAll.js
+++ src/pages/api/images/readAll.js
@@ -1,17 +1,10 @@
 import { db, AdminKeys, like, Images } from "astro:db";
-import { v2 as cloudinary } from 'cloudinary';
-import { env } from "process";
 
 export const prerender = false;
 
-cloudinary.config({ 
-  cloud_name: import.meta.env.CLOUDINARY_CLOUD_NAME,
-  api_key: import.meta.env.CLOUDINARY_API_KEY,
-  api_secret: import.meta.env.CLOUDINARY_API_SECRET
-});
 
 export const POST = async ({ request }) => {
-  const { key, image } = await request.json();
+  const { key } = await request.json();
 
   if (!key) {
     return new Response(
@@ -41,32 +34,12 @@ export const POST = async ({ request }) => {
       );
     }
 
-    const uploadResult = await cloudinary.uploader.upload(`data:image/jpeg;base64,${image}`, {
-      public_id: `uploaded_image_${Date.now()}`,
-      folder: "images" // Opcional: especifica la carpeta en Cloudinary
-    });
-
-    if (!uploadResult || !uploadResult.secure_url) {
-      return new Response(
-        JSON.stringify({
-          message: "Error al subir la imagen a Cloudinary",
-        }),
-        {
-          status: 500,
-        }
-      );
-    }
-
-    const imageUrl = uploadResult.secure_url;
-
-    await db.insert(Images).values({
-      image: imageUrl,
-    });
+    const images = await db.select().from(Images)
 
     return new Response(
       JSON.stringify({
         message: "Imagen subida",
-        url: imageUrl,
+        images: images,
       }),
       {
         status: 200,
